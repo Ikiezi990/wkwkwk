b2172e418b0295c7af0990a63ee40eb4a595fa2b
Fitri Salwa <fitrisalwa09@gmail.com>
2023-02-18 03:07:08 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+use Simplexi\Greetr\Greetr;
+
+Route::get('/greet/{name}', function ($sName) {
+    $oGreetr = new Greetr();
+    return $oGreetr->greet($sName);
+});
